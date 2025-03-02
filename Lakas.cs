a155e3ff01ee517c111d2225df65c97598decb7b
kissagnesa <sindzse88@gmail.com>
2025-03-02 15:06:18 +0100

    c

---
+++ Lakas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakokCLI
+{
+    public class Lakas
+    {
+        public string Cim { get; private set; }
+        public int LakokSzama { get; private set; }
+        public string LakokNeve { get; private set; }
+        public int Terulet { get; private set; }
+        public int Tartozas { get; private set; }
+
+        public Lakas(string sor)
+        {
+            string[] adatok = sor.Split(',');
+            Cim = adatok[0];
+            LakokSzama = int.Parse(adatok[1]);
+            LakokNeve = adatok[2];
+            Terulet = int.Parse(adatok[3]);
+        }
+
+        public bool Tartozik()
+        {
+            return Tartozas != 0;
+        }
+
+        public bool Tulzsufolt()
+        {
+            int maxLakok = (Terulet - 40) / 4;
+            return LakokSzama > maxLakok;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,77 @@
-﻿using System;
+﻿using LakokCLI;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LakokWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
+
     public partial class MainWindow : Window
     {
+        public List<Lakas> LakokLista { get; set; }
+        private string filePath = "lakok.csv";
+
         public MainWindow()
         {
             InitializeComponent();
+            LakokLista = new List<Lakas>();
+            Betoltes();
+            dgLakok.ItemsSource = LakokLista;
+        }
+
+        private void Betoltes()
+        {
+            if (File.Exists(filePath))
+            {
+                var sorok = File.ReadAllLines(filePath);
+                foreach (var sor in sorok)
+                {
+                    var adatok = sor.Split(';');
+                    LakokLista.Add(new Lakas
+                    {
+                        Cim = adatok[0],
+                        LakokSzama = int.Parse(adatok[1]),
+                        LakokNeve = adatok[2],
+                        Terulet = int.Parse(adatok[3]),
+                    });
+                }
+            }
+        }
+
+        private void btnBevitel_Click(object sender, RoutedEventArgs e)
+        {
+            var ujLakas = new Lakas
+            {
+                Cim = txtCim.Text,
+                LakokSzama = int.Parse(txtLakokSzama.Text),
+                LakokNeve = txtNev.Text,
+                Terulet = int.Parse(txtTerulet.Text),
+                Tartozas = 0
+            };
+            LakokLista.Add(ujLakas);
+            dgLakok.Items.Refresh();
+            Mentes();
+        }
+
+        private void btnKiegyenlites_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgLakok.SelectedItem is Lakas kijelolt)
+            {
+                kijelolt.Tartozas = 0;
+                dgLakok.Items.Refresh();
+                Mentes();
+                MessageBox.Show("Tartozás kiegyenlítve!");
+            }
+        }
+
+        private void Mentes()
+        {
+            var sorok = LakokLista.Select(l => $"{l.Cim};{l.LakokSzama};{l.LakokNeve};{l.Terulet};{l.Tartozas}");
+            File.WriteAllLines(filePath, sorok);
         }
     }
 }
